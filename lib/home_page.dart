e6381a92e47e9385b129fce9d915546f3fe4a0ec
King <valvuong2201@gmail.com>
2022-02-22 01:10:24 +0700

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:parallax/exhibition_bottom_sheet.dart';
+import 'package:parallax/sliding_cards.dart';
+import 'package:parallax/tabs.dart';
+import 'package:flutter/material.dart';
+
+import 'exhibition_bottom_sheet.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 8),
+                Header(),
+                SizedBox(height: 40),
+                Tabs(),
+                SizedBox(height: 8),
+                SlidingCardsView(),
+              ],
+            ),
+          ),
+          ExhibitionBottomSheet(),
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Text(
+        'Shenzhen',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
